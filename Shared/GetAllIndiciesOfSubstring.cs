a186edbf5ace5d70cdb26e62ce6fe8d7cf2ac5f2
Eric Friedman <ericleefriedman@gmail.com>
2024-12-03 09:52:14 -0800

    add shared projectg

---
+++ Shared/GetAllIndiciesOfSubstring.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared;
+
+public static class ListExtensions
+{
+    public static List<int> GetAllIndiciesOfSubstring(this string sourceString, string stringToFind)
+    {
+        List<int> allIndicies = [];
+        int searchIndex = 0;
+        while (searchIndex < sourceString.Length)
+        {
+            int substringIndex = sourceString.IndexOf(stringToFind, searchIndex);
+            if (substringIndex < 0) break;
+        
+            allIndicies.Add(substringIndex);
+            searchIndex = (substringIndex + stringToFind.Length);
+        }
+        return allIndicies;
+    }   
+}
